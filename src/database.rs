24df8d2a646be8564bebe1549382e64063b11a1c
Alex Trotta <ahajha@gmail.com>
2021-12-18 00:41:31 -0500

    move label pruning and id assignment to read() instead of helper

--- src/database.rs
+++ src/database.rs
@@ -162,8 +162,10 @@ impl Database {
 		
 		let (mut node_labels, mut edge_labels) = Self::count_labels(&trees);
 
-		let trees = Self::prune_infrequent_nodes_and_edges(trees, &mut node_labels,
-			&mut edge_labels, min_freq);
+		Self::prune_and_assign_ids(&mut node_labels, min_freq);
+		Self::prune_and_assign_ids(&mut edge_labels, min_freq);
+
+		let trees = Self::prune_infrequent_nodes_and_edges(trees, &node_labels, &edge_labels, min_freq);
 		
 		Ok(Database {
 			trees,
@@ -187,12 +189,10 @@ impl Database {
 	}
 
 	fn prune_infrequent_nodes_and_edges(mut trees: Vec<RawInputGraph>,
-		node_labels: &mut HashMap<InputNodeLabel, DatabaseLabelCounts>,
-		edge_labels: &mut HashMap<CombinedInputLabel, DatabaseLabelCounts>,
+		node_labels: &HashMap<InputNodeLabel, DatabaseLabelCounts>,
+		edge_labels: &HashMap<CombinedInputLabel, DatabaseLabelCounts>,
 		min_freq: types::Frequency)
 		-> Vec<DatabaseTree> {
-		Self::prune_and_assign_ids(node_labels, min_freq);
-		Self::prune_and_assign_ids(edge_labels, min_freq);
 		
 		trees.iter_mut().enumerate().map(|(tid, tree)| {
 			let nodes = &tree.nodes;
